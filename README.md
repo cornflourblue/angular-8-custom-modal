747d7255ce691736d31fc4aadc1a071cc40f9200
jwatmore <jason.watmore@hotmail.com>
2019-07-10 12:17:04 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# angular-8-custom-modal
+
+Angular 8 Custom Modal with Angular CLI
+
---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { TestPageComponent } from './test-page';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'test-page', component: TestPageComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
